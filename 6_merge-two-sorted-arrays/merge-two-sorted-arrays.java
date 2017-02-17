dbacb009ca32e98d61d37a80182d4838586ced1a
KevinZh92 <monsterzpc@gmail.com>
2017-02-16 20:57:53 -0500

    algorithm

---
+++ 6_merge-two-sorted-arrays/merge-two-sorted-arrays.java
@@ -0,0 +1,45 @@
+/*
+@Copyright:LintCode
+@Author:   zhou21
+@Problem:  http://www.lintcode.com/problem/merge-two-sorted-arrays
+@Language: Java
+@Datetime: 17-01-24 04:00
+*/
+
+class Solution {
+    /**
+     * @param A and B: sorted integer array A and B.
+     * @return: A new sorted integer array
+     */
+    public int[] mergeSortedArray(int[] A, int[] B) {
+       if (A == null) {
+           return B;
+       }
+       
+       if (B == null) {
+           return A;
+       } 
+       
+       int[] merged = new int[A.length + B.length];
+       
+       int n = 0;
+       int m = 0;
+       int index = 0;
+       
+       while (n < A.length && m < B.length) {
+           if(A[n] < B[m]) {
+               merged[index++] = A[n++];
+           }else {
+               merged[index++] = B[m++];
+           }
+       }   
+       while (n < A.length) {
+           merged[index++] = A[n++];
+       }
+       
+       while (m < B.length) {
+           merged[index++] = B[m++];
+       }
+        return merged;   
+    }
+}
---
+++ 6_merge-two-sorted-arrays/note.md
@@ -0,0 +1,9 @@
+```
+@Copyright:LintCode
+@Author:   zhou21
+@Problem:  http://www.lintcode.com/problem/merge-two-sorted-arrays
+@Language: Markdown
+@Datetime: 17-01-24 04:00
+```
+
+1.没什么难度，参考Merge Sorted Array
