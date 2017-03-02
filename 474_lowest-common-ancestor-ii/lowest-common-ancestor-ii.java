22f781c19ab4ad009dd93fee151e19f6382e9f97
KevinZh92 <monsterzpc@gmail.com>
2017-03-02 08:00:09 -0500

    Update

---
+++ 474_lowest-common-ancestor-ii/lowest-common-ancestor-ii.java
@@ -0,0 +1,54 @@
+/*
+@Copyright:LintCode
+@Author:   zhou21
+@Problem:  http://www.lintcode.com/problem/lowest-common-ancestor-ii
+@Language: Java
+@Datetime: 17-02-03 00:10
+*/
+
+/**
+ * Definition of ParentTreeNode:
+ * 
+ * class ParentTreeNode {
+ *     public ParentTreeNode parent, left, right;
+ * }
+ */
+public class Solution {
+    /**
+     * @param root: The root of the tree
+     * @param A, B: Two node in the tree
+     * @return: The lowest common ancestor of A and B
+     */
+    public ParentTreeNode lowestCommonAncestorII(ParentTreeNode root,
+                                                 ParentTreeNode A,
+                                                 ParentTreeNode B) {
+    
+    
+    ArrayList<ParentTreeNode> pathA = getpath2node(A);
+    ArrayList<ParentTreeNode> pathB = getpath2node(B);
+    
+    int indexA = pathA.size() - 1;
+    int indexB = pathB.size() - 1;
+    
+    ParentTreeNode lca = null;
+    while (indexA >= 0 && indexB >= 0) {
+        if (pathA.get(indexA) != pathB.get(indexB)) {
+            break;
+        }
+        lca = pathB.get(indexB);
+        indexA--;
+        indexB--;
+        
+    }
+        return lca;                                        
+    }
+    
+    private ArrayList<ParentTreeNode> getpath2node(ParentTreeNode root) {
+        ArrayList<ParentTreeNode> path = new ArrayList<ParentTreeNode>();
+        while (root != null) {
+            path.add(root);
+            root = root.parent;
+        }
+        return path;
+    }
+}
---
+++ 474_lowest-common-ancestor-ii/note.md
@@ -0,0 +1,9 @@
+```
+@Copyright:LintCode
+@Author:   zhou21
+@Problem:  http://www.lintcode.com/problem/lowest-common-ancestor-ii
+@Language: Markdown
+@Datetime: 17-02-03 00:10
+```
+
+增加了父节点，所以可以从两个点回溯上去，注意灵活思考即可，无难度，注意corner case
