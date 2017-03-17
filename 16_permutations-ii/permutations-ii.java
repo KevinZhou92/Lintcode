ca4683a4706416d5dfeb632303b121608c21197f
KevinZh92 <monsterzpc@gmail.com>
2017-03-17 19:58:53 -0400

    U

--- 16_permutations-ii/permutations-ii.java
+++ 16_permutations-ii/permutations-ii.java
@@ -3,7 +3,7 @@
 @Author:   zhou21
 @Problem:  http://www.lintcode.com/problem/permutations-ii
 @Language: Java
-@Datetime: 17-01-19 15:56
+@Datetime: 17-02-25 15:00
 */
 
 class Solution {
@@ -12,45 +12,35 @@ class Solution {
      * @return: A list of unique permutations.
      */
     public List<List<Integer>> permuteUnique(int[] nums) {
-        ArrayList<List<Integer>> results = new ArrayList<List<Integer>>();
-           
+        List<List<Integer>> results = new ArrayList<>();
         if (nums == null) {
-           return results;
-       } 
-    
-        if (nums.length == 0) {
-            results.add(new ArrayList<Integer>());
             return results;
         }
         
-        int[] visited = new int[nums.length];
-        for (int i = 0; i< nums.length; i++) {
-            visited[i] = 0;
-        }
         Arrays.sort(nums);
-        search(visited, nums,new ArrayList<Integer>(), results);
+        List<Integer> path = new ArrayList<>();
+        boolean[] visited = new boolean[nums.length];
+        dfs(results, path, nums, visited);
         return results;
-    }
+    } 
     
-    private void search(int[] visited, int[] nums, ArrayList<Integer> set, ArrayList<List<Integer>> results) {
-        if (set.size() == nums.length) {
-            results.add(new ArrayList<Integer>(set));
+    private void dfs(List<List<Integer>> results, List<Integer> path, int[] nums, boolean[] visited) {
+        if (path.size() == nums.length) {
+            results.add(new ArrayList(path));
             return;
         }
         
         for (int i = 0; i < nums.length; i++) {
-            if (visited[i] == 1 || i !=0 && nums[i] == nums[i-1] && visited[i-1] == 0) {
+            if (i != 0 && nums[i - 1] == nums[i] && visited[i - 1] == false || visited[i] == true) {
                 continue;
             }
-            visited[i] = 1;
-            set.add(nums[i]);
-            search(visited, nums, set, results);
-            visited[i] = 0;
-            set.remove(set.size() - 1);
+            path.add(nums[i]);
+            visited[i] = true;
+            dfs(results, path, nums, visited);
+            visited[i] = false;
+            path.remove(path.size() - 1);
         }
     }
-    
-    
 }
 
 
