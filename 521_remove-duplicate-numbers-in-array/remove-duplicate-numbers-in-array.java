47984f6969c9881a8c6a93a1b180b7cbd1548f9a
KevinZh92 <monsterzpc@gmail.com>
2017-03-29 23:42:53 -0400

    Update

---
+++ 521_remove-duplicate-numbers-in-array/remove-duplicate-numbers-in-array.java
@@ -0,0 +1,30 @@
+/*
+@Copyright:LintCode
+@Author:   zhou21
+@Problem:  http://www.lintcode.com/problem/remove-duplicate-numbers-in-array
+@Language: Java
+@Datetime: 17-03-28 17:50
+*/
+
+public class Solution {
+    /**
+     * @param nums an array of integers
+     * @return the number of unique integers
+     */
+    public int deduplication(int[] nums) {
+        // Write your code here
+        if (nums.length == 0) {
+            return 0;
+        }
+        
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            set.add(nums[i]);
+        }
+        int index = 0;
+        for (int i : set) {
+            nums[index++] = i;
+        }
+        return index ;
+    }
+}
