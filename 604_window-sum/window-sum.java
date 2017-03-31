2376668ef6b790e18484ce55a90e5bd0565bb567
KevinZh92 <monsterzpc@gmail.com>
2017-03-31 10:51:56 -0400

    Update

--- 604_window-sum/window-sum.java
+++ 604_window-sum/window-sum.java
@@ -3,7 +3,7 @@
 @Author:   zhou21
 @Problem:  http://www.lintcode.com/problem/window-sum
 @Language: Java
-@Datetime: 17-03-16 20:06
+@Datetime: 17-03-29 01:52
 */
 
 public class Solution {
@@ -12,20 +12,17 @@ public class Solution {
      * @return the sum of the element inside the window at each moving.
      */
     public int[] winSum(int[] nums, int k) {
-         if (nums == null || nums.length < k || k <= 0)
+        // write your code here
+        if (nums.length == 0 ) {
             return new int[0];
-            
-        int[] result = new int[nums.length - k + 1];
-        
-        for (int i = 0; i < k; i++) {
-            result[0] += nums[i];
         }
-        
-        for (int i = 1; i < nums.length - k + 1; i++) {
-            result[i] = result[i - 1] - nums[i - 1] + nums[i + k - 1];
+        int[] res = new int[nums.length - k + 1];
+        for (int i = 0; i < nums.length - k + 1; i++) {
+            for (int j = i; j < i + k; j++) {
+                res[i] += nums[j]; 
+            }
         }
-        
-        return result;
+        return res;
     }
 }
 
