1eaf747a14ade93d4e773c0eeaf62d8548d1d265
KevinZh92 <monsterzpc@gmail.com>
2017-04-15 20:18:45 -0400

    Update

---
+++ 596_minimum-subtree/211_string-permutation/string-permutation.java
@@ -0,0 +1,32 @@
+/*
+@Copyright:LintCode
+@Author:   zhou21
+@Problem:  http://www.lintcode.com/problem/string-permutation
+@Language: Java
+@Datetime: 17-03-02 01:05
+*/
+
+public class Solution {
+    /**
+     * @param A a string
+     * @param B a string
+     * @return a boolean
+     */
+    public boolean stringPermutation(String A, String B) {
+        int[] count = new int[1000];
+        
+        for (int i = 0; i < A.length(); i++) {
+            count[(int)A.charAt(i)]++;
+        }
+        for (int i = 0; i < B.length(); i++) {
+            count[(int)B.charAt(i)]--;
+        }
+        
+        for (int i = 0; i < count.length; i++) {
+            if (count[i] != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
