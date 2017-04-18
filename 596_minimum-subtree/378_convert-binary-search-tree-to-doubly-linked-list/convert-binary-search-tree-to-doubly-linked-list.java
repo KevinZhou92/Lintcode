62dff2759bcc5911da82ec1daabc09da84e7f1a3
KevinZh92 <monsterzpc@gmail.com>
2017-04-17 23:44:53 -0400

    Update

---
+++ 596_minimum-subtree/378_convert-binary-search-tree-to-doubly-linked-list/convert-binary-search-tree-to-doubly-linked-list.java
@@ -0,0 +1,65 @@
+/*
+@Copyright:LintCode
+@Author:   zhou21
+@Problem:  http://www.lintcode.com/problem/convert-binary-search-tree-to-doubly-linked-list
+@Language: Java
+@Datetime: 17-02-08 05:18
+*/
+
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ * Definition for Doubly-ListNode.
+ * public class DoublyListNode {
+ *     int val;
+ *     DoublyListNode next, prev;
+ *     DoublyListNode(int val) {
+ *         this.val = val;
+ *         this.next = this.prev = null;
+ *     }
+ * }
+ */ 
+public class Solution {
+    /**
+     * @param root: The root of tree
+     * @return: the head of doubly list node
+     */
+    public DoublyListNode bstToDoublyList(TreeNode root) {  
+        if (root == null) {
+            return null;
+        }
+        
+        DoublyListNode head = new DoublyListNode(0);
+        DoublyListNode tracker = head;
+        
+        Stack<TreeNode> pool = new Stack<TreeNode>();
+        TreeNode node = root;
+        pool.push(node);
+        
+        while(!pool.empty()) {
+            while (node != null && node.left != null) {
+                node = node.left;
+                pool.push(node);
+            }
+            
+            node = pool.pop();
+            DoublyListNode current = new DoublyListNode(node.val);
+            tracker.next = current;
+            current.prev = current;
+            tracker = current;
+            
+            node = node.right;
+            if (node != null) {
+               pool.push(node); 
+            }
+        }
+        return head.next;
+    }
+}
