5f017fd8c883a9ca06c3fb2cb6fb675f7f60c983
KevinZh92 <monsterzpc@gmail.com>
2017-04-21 23:33:51 -0400

    U

---
+++ 596_minimum-subtree/604_window-sum/window-sum.java
@@ -0,0 +1,28 @@
+/*
+@Copyright:LintCode
+@Author:   zhou21
+@Problem:  http://www.lintcode.com/problem/window-sum
+@Language: Java
+@Datetime: 17-03-29 01:52
+*/
+
+public class Solution {
+    /**
+     * @param nums a list of integers.
+     * @return the sum of the element inside the window at each moving.
+     */
+    public int[] winSum(int[] nums, int k) {
+        // write your code here
+        if (nums.length == 0 ) {
+            return new int[0];
+        }
+        int[] res = new int[nums.length - k + 1];
+        for (int i = 0; i < nums.length - k + 1; i++) {
+            for (int j = i; j < i + k; j++) {
+                res[i] += nums[j]; 
+            }
+        }
+        return res;
+    }
+}
+
