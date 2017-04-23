a06c952c4a727d39e6d29c6d5e6e100952016f44
KevinZh92 <monsterzpc@gmail.com>
2017-04-22 23:57:38 -0400

    U

---
+++ 596_minimum-subtree/63_search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -0,0 +1,27 @@
+/*
+@Copyright:LintCode
+@Author:   zhou21
+@Problem:  http://www.lintcode.com/problem/search-in-rotated-sorted-array-ii
+@Language: Java
+@Datetime: 17-01-24 01:17
+*/
+
+public class Solution {
+    /** 
+     * param A : an integer ratated sorted array and duplicates are allowed
+     * param target :  an integer to be search
+     * return : a boolean 
+     */
+    public boolean search(int[] A, int target) {
+        if (A.length == 0 || A == null) {
+            return false;
+        }
+        
+        for (int i = 0; i < A.length; i++){
+            if (A[i] == target) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
