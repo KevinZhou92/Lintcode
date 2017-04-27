b04c31fa651fb3e3992ef77bc2c0e761fef17909
KevinZh92 <monsterzpc@gmail.com>
2017-04-27 00:51:17 -0400

    U

---
+++ 596_minimum-subtree/619_binary-tree-longest-consecutive-sequence-iii/binary-tree-longest-consecutive-sequence-iii.java
@@ -0,0 +1,55 @@
+/*
+@Copyright:LintCode
+@Author:   zhou21
+@Problem:  http://www.lintcode.com/problem/binary-tree-longest-consecutive-sequence-iii
+@Language: Java
+@Datetime: 17-02-06 04:41
+*/
+
+/**
+ * Definition for a multi tree node.
+ * public class MultiTreeNode {
+ *     int val;
+ *     List<TreeNode> children;
+ *     MultiTreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    /**
+     * @param root the root of k-ary tree
+     * @return the length of the longest consecutive sequence path
+     */
+     
+    private class Result {
+        int up, down, max;
+        public Result(int up, int down, int max) {
+            this.up = up;
+            this.down = down;
+            this.max = max;
+        }
+    } 
+    public int longestConsecutive3(MultiTreeNode root) {
+        return helper(root).max;
+    }
+    
+    private Result helper(MultiTreeNode root) {
+        if (root == null) {
+            return new Result(0, 0, 0);
+        }
+        
+        int up = 0, down = 0, max = 1;
+        for (MultiTreeNode child : root.children) {
+            Result node = helper(child);
+            if (root.val == child.val + 1) {
+                up = Math.max(up, node.up + 1);
+            }
+            if (root.val == child.val - 1) {
+                down = Math.max(down, node.down + 1);
+            }
+            max = Math.max(max, node.max);
+        }
+         max = Math.max(up + down + 1, max);
+        
+        return new Result(up, down ,max);
+    }
+}
