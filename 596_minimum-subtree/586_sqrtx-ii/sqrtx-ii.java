456ac1253ce4ac570b04835c0d89bd53de73ca13
KevinZh92 <monsterzpc@gmail.com>
2017-05-03 22:25:27 -0400

    U

---
+++ 596_minimum-subtree/586_sqrtx-ii/sqrtx-ii.java
@@ -0,0 +1,38 @@
+/*
+@Copyright:LintCode
+@Author:   zhou21
+@Problem:  http://www.lintcode.com/problem/sqrtx-ii
+@Language: Java
+@Datetime: 17-01-28 20:46
+*/
+
+public class Solution {
+    /**
+     * @param x a double
+     * @return the square root of x
+     */
+    public double sqrt(double x) {
+        if (x == 0 || x == 1) {
+            return x;
+        }
+        
+        double start = 0;
+        double end = x > 1 ? x : 1;
+        while (end - start >= 1e-10) {
+            double mid = start + (end - start) / 2.0;
+            
+            if (mid * mid < x) {
+                start = mid;
+            }else if(mid * mid == x) {
+                return mid;
+            }else {
+                end = mid;
+            }
+        } 
+        
+        if (start * start <= x) {
+                return start;
+            }
+        return end;
+    }
+}
