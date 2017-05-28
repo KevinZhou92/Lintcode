e548c9c26ba9f539a6f4156768a808a0abae315a
KevinZh92 <monsterzpc@gmail.com>
2017-05-29 00:30:58 +0800

    U

---
+++ 114_unique-paths/unique-paths.java
@@ -0,0 +1,31 @@
+/*
+@Copyright:LintCode
+@Author:   zhou21
+@Problem:  http://www.lintcode.com/problem/unique-paths
+@Language: Java
+@Datetime: 17-04-26 03:35
+*/
+
+public class Solution {
+    /**
+     * @param n, m: positive integer (1 <= n ,m <= 100)
+     * @return an integer
+     */
+    public int uniquePaths(int m, int n) {
+        // write your code here 
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            dp[i][0] = 1;
+        }
+        for (int i = 0; i < n; i++) {
+            dp[0][i] = 1;
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j ++) {
+                dp[i][j] = dp[i-1][j] + dp[i][j - 1];
+            }
+        }
+        
+        return dp[m - 1][n - 1];
+    }
+}
