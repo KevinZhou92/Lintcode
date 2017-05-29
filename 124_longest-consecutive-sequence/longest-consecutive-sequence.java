fa368905f69013fbc27c37e5f9915d0b2536455a
KevinZh92 <monsterzpc@gmail.com>
2017-05-30 02:31:32 +0800

    U

---
+++ 124_longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -0,0 +1,39 @@
+/*
+@Copyright:LintCode
+@Author:   zhou21
+@Problem:  http://www.lintcode.com/problem/longest-consecutive-sequence
+@Language: Java
+@Datetime: 17-04-25 14:14
+*/
+
+public class Solution {
+    /**
+     * @param nums: A list of integers
+     * @return an integer
+     */
+    public int longestConsecutive(int[] num) {
+        // write you code here
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < num.length; i++) {
+            set.add(num[i]);
+        }
+        
+        int maxlength = 1;
+        for (int i = 0; i < num.length; i++) {
+            int up = num[i] + 1;
+            while (set.contains(up)) {
+                set.remove(up);
+                up += 1;
+            }
+            
+            int down = num[i] - 1;
+            while (set.contains(down)) {
+                set.remove(down);
+                down -= 1;
+            }
+            
+            maxlength = Math.max(maxlength, up - down - 1);
+        }
+        return maxlength;
+    }
+}
