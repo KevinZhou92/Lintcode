aacbef5f8ee4803e5dc39d21337ca4bae9f289b7
KevinZh92 <monsterzpc@gmail.com>
2017-06-01 09:19:52 +0800

    U

---
+++ 272_climbing-stairs-ii/climbing-stairs-ii.java
@@ -0,0 +1,29 @@
+/*
+@Copyright:LintCode
+@Author:   zhou21
+@Problem:  http://www.lintcode.com/problem/climbing-stairs-ii
+@Language: Java
+@Datetime: 17-05-02 19:18
+*/
+
+public class Solution {
+    /**
+     * @param n an integer
+     * @return an integer
+     */
+    public int climbStairs2(int n) {
+        // Write your code here
+        int[] dp = new int[n + 1];
+        
+        dp[0] = 1;
+        
+        for (int i = 0; i <= n; i++) {
+            for (int j = 1; j <=3; j++) {
+                if (i >=j) {
+                    dp[i] += dp[i - j];
+                }
+            }
+        }
+        return dp[n];
+    }
+}
