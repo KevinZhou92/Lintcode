2a8c79981665d922040cbca10617c653bfff3914
KevinZh92 <monsterzpc@gmail.com>
2017-06-09 09:20:27 +0800

    U

--- 594_strstr-ii/strstr-ii.java
+++ 594_strstr-ii/strstr-ii.java
@@ -3,7 +3,7 @@
 @Author:   zhou21
 @Problem:  http://www.lintcode.com/problem/strstr-ii
 @Language: Java
-@Datetime: 17-01-18 19:16
+@Datetime: 17-04-12 04:37
 */
 
 public class Solution {
@@ -12,52 +12,59 @@ public class Solution {
      * @param target a target string
      * @return an integer as index
      */
+    private int HASH_SIZE = 10000;
+    
+    public int hash_code(String input) {
+        int hash_value = 0;
+        for(int i = 0; i < input.length(); i++) {
+            hash_value = ((hash_value * 33) % HASH_SIZE + input.charAt(i)) % HASH_SIZE;
+        }
+        return hash_value;
+    }
     public int strStr2(String source, String target) {
-       final int BASE = 100000;
-       if (source == null || target == null) {
-           return -1;
-       }
-       
-       if (target.equals("")) {
-           return 0;
-       }
-       
-       int m = target.length();
-       
-       //power of the first letter
-       
-       int power = 1;
-       for (int i = 0; i < m ; i++) {
-           power = power * 31 ;
-       }
-       
-       //hashcode for target
-       int targetHash = 0;
-       for (int i = 0; i < m; i++) {
-           targetHash = targetHash * 31 + target.charAt(i) ;
-       }
-       
-       //hashcode at source
-       int hashCode = 0;
-       for (int i = 0; i < source.length(); i++) {
-            hashCode = (hashCode * 31 + source.charAt(i)) ;
-        
-        if (i < m -1 ) {
-            continue;
+        // Write your code here
+        if(target == null) {
+            return -1;
         }
-        if (i >= m ) {
-            hashCode = hashCode - (power * source.charAt(i - m)) ;
-            //if(hashCode < 0){
-              //  hashCode += BASE;
-            //}
+        int m = target.length();
+        if(m == 0) {
+            return 0;
         }
         
-        if (hashCode == targetHash) {
-            if (source.substring(i - m + 1, i + 1).equals(target)) {
-                return i - m + 1;
+        if(source == null) {
+            return -1;
+        }
+        int n = source.length();
+        if(n == 0) {
+            return -1;
+        }
+        
+        int input_hash = hash_code(target);
+        
+        int factor = 1;
+        for (int i = 0; i < target.length(); i++) {
+            factor = (factor * 33) % HASH_SIZE;
+        }
+        
+        int temp = 0;
+        int i = 0;
+       
+        for (i = 0; i < source.length(); i++) {
+            temp = ((temp * 33) % HASH_SIZE + source.charAt(i)) % HASH_SIZE;
+            
+            if (i >= target.length()) {
+                temp = temp - factor * source.charAt(i - target.length()) % HASH_SIZE;
+                if (temp < 0) {
+                    temp += HASH_SIZE;
+                }
+            }
+
+            if (i >= m - 1 && temp == input_hash) {
+                if (source.substring( i - m + 1, i + 1).equals(target)) {
+                    return i - m + 1;
+                }
             }
         }
-    }
-       return -1;
+        return -1;
     }
 }
