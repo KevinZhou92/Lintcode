9a8726517a3f56dae679e2045fe99dbdee072b3b
KevinZh92 <monsterzpc@gmail.com>
2017-06-16 23:52:51 +0800

    U

---
+++ 70_binary-tree-level-order-traversal-ii/binary-tree-level-order-traversal-ii.java
@@ -0,0 +1,57 @@
+/*
+@Copyright:LintCode
+@Author:   zhou21
+@Problem:  http://www.lintcode.com/problem/binary-tree-level-order-traversal-ii
+@Language: Java
+@Datetime: 17-05-01 20:31
+*/
+
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+ 
+ 
+public class Solution {
+    /**
+     * @param root: The root of binary tree.
+     * @return: buttom-up level order a list of lists of integer
+     */
+    public ArrayList<ArrayList<Integer>> levelOrderBottom(TreeNode root) {
+        // write your code here
+        ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        
+        
+        while (!q.isEmpty()) {
+            ArrayList<Integer> path = new ArrayList<>();
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode cur = q.poll();
+                if (cur.left != null) {
+                    q.offer(cur.left);
+                }
+                if (cur.right != null) {
+                    q.offer(cur.right);
+                }
+                path.add(cur.val);
+            }
+            res.add(path);
+        }
+        
+        Collections.reverse(res);
+        return res;
+    }
+}
